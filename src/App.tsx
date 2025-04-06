cc4f159c58cb4eac9cc6bf843e81839a436116ad
Utkarsh-2525 <2101641530158@psit.ac.in>
2025-04-06 23:14:19 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.tsx";
+
+/**
+ * @copyright 2025 M.Utkarsh
+ * @license Apache-2.0
+ */
+const App = () => {
+    return (
+        <Header/>
+    );
+}
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindScrollbar = require('tailwind-scrollbar');
+
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [tailwindScrollbar],
+};
