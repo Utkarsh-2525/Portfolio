d3b422150490c9c069c41ca826d11f767776138b
Utkarsh-2525 <2101641530158@psit.ac.in>
2025-04-14 01:50:28 +0530

    HomePage updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import {ButtonOutline, ButtonPrimary} from "./Button.tsx";
+
 /**
  * @copyright 2025 M.Utkarsh
  * @license Apache-2.0
@@ -11,24 +13,31 @@ const Hero = () => {
                         <figure className="img-box w-9 h-9 rounded-lg">
                             <img src="/images/photo.jpg" alt="M.Utkarsh" width={40} height={40} className='img-cover'/>
                         </figure>
-                        <div className="">
-                            <span className="">
-                                <span className=""></span>
+                        <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                            <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                                <span className="absolute inset-0 w-2 h-2 rounded-full bg-emerald-400 animate-ping"></span>
                             </span>
                             Available for work.
                         </div>
                     </div>
-                    <h2 className="">
+                    <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
                         Developing Full Stack projects for personal and professional use.
                     </h2>
-                    <div className="">
-                        ButtonPrimary
-                        ButtonOutline
+                    <div className="flex items-center gap-3">
+                        <ButtonPrimary
+                            label='Download CV'
+                            icon='download'
+                        />
+                        <ButtonOutline
+                        href='#about'
+                        label='Scroll down'
+                        icon='arrow_downward'
+                        />
                     </div>
                 </div>
-                <div className="">
-                    <figure className="w-full">
-                        <img src="/images/Photo%20IIT%202.png"
+                <div className="hidden lg:block">
+                    <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-30% via-sky-400/55 to-65% rounded-[60px]">
+                        <img src="/images/Photo%20IIT%202.png" className='w-full'
                         width={500} height={800} alt="M.Utkarsh"/>
                     </figure>
                 </div>
