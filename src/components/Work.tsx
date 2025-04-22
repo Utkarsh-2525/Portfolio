b21332ee99d7034e0d668b589cd8cd189da28e4b
Utkarsh-2525 <2101641530158@psit.ac.in>
2025-04-22 23:29:11 +0530

    Work section completed

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -6,6 +6,12 @@ import ProjectCard from "./ProjectCard.tsx";
 
 const works = [
     {
+        imgSrc: '/images/projects/iitk-vlabs.jpg',
+        title: 'Employee Resource Planning(ERP) web application',
+        tags: ['ERP', 'Virtual Labs', 'IITK', 'API'],
+        projectLink: 'https://erp.ebootathon.com/'
+    },
+    {
         imgSrc: '/images/projects/budgetkart.png',
         title: 'Full stack E-Commerce Website',
         tags: ['API', 'Spring Boot', 'Angular'],
@@ -15,32 +21,26 @@ const works = [
         imgSrc: '/images/projects/readers-paradise.png',
         title: 'Readers Paradise Library app',
         tags: ['API', 'SPA'],
-        projectLink: 'https://pixstock-official.vercel.app/'
+        projectLink: 'https://github.com/Utkarsh-2525/Readers-Paradise-The-Library-App-.git'
     },
     {
-        imgSrc: '/images/projects/iitk-vlabs.png',
-        title: 'Recipe app',
-        tags: ['Development', 'API'],
-        projectLink: ''
+        imgSrc: '/images/projects/atm.jpg',
+        title: 'ATM Simulation Application',
+        tags: ['Desktop-App', 'Development'],
+        projectLink: 'https://github.com/Utkarsh-2525/ATM-Simulation-App.git'
     },
-    {
-        imgSrc: '/images/project-4.jpg',
-        title: 'Real state website',
-        tags: ['Web-design', 'Development'],
-        projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-        imgSrc: '/images/project-5.jpg',
-        title: 'eCommerce website',
-        tags: ['eCommerce', 'Development'],
-        projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-        imgSrc: '/images/project-6.jpg',
-        title: 'vCard Personal portfolio',
-        tags: ['Web-design', 'Development'],
-        projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-    },
+    // {
+    //     imgSrc: '/images/project-5.jpg',
+    //     title: 'eCommerce website',
+    //     tags: ['eCommerce', 'Development'],
+    //     projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+    // },
+    // {
+    //     imgSrc: '/images/project-6.jpg',
+    //     title: 'vCard Personal portfolio',
+    //     tags: ['Web-design', 'Development'],
+    //     projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+    // },
 ];
 
 const Work = () => {
@@ -50,13 +50,14 @@ const Work = () => {
                 <h2 className="headline-2 mb-8">
                     My Portfolio Highlights
                 </h2>
-                <div className="">
+                <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
                     {
                         works.map(({imgSrc, title, tags, projectLink}, key) => (
                             <ProjectCard key={key}
                             imgSrc={imgSrc}
                             title={title}
                             tags={tags}
+                            projectLink={projectLink}
                             />
                         ))
                     }
